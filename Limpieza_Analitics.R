a4f6f5fad5ddf6655a163363d3e86a3aab937d68
Gugui <guguiderqui@gmail.com>
2023-11-20 19:22:22 +0100

    Creación de la Rama para que Angela haga la limpieza de Analiticas

---
+++ Limpieza_Analitics.R
@@ -0,0 +1,7 @@
+ANALITIC <- read.csv("D:/gugui/Documentos/Universidad/TFG/Analiticas.csv", sep = ";", header = TRUE)
+
+summary(ANALITIC)
+
+# - Creatinina sérica (a más alto, peor evolución)
+# - Cociente albuminuria/creatinina en orina (a más alto, peor evolución)
+# - Filtrado glomerular estimado (fórmula CKD-EPI) (a más bajo, peor evolución)
